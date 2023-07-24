f993108595cec88fad2e77eddc52c8c9646bb327
Gingene <ginx2618@gmail.com>
2023-07-25 00:02:20 +0800

    complete storePage and fix some problem

--- main.js
+++ main.js
@@ -46,17 +46,48 @@ function removecategoryMenu() {
   });
 }
 
-// function opticalGlasses() {
-//   return [
-//     {
-//       id: "001",
-//       name: "BJ41600S",
-//       price: "NTD3,490",
-//       image: "../assets/images/product-1.png",
-//     }
-//   ]
-// }
+getCurrentFileName() === "store.html" ? storePage() : console.log("not store");
 
-// function rednerOptical() {
+function storePage() {
+  const storeCards = document.querySelectorAll(".store-list-card");
+  const locationInput = document.querySelector("#locationInput");
 
-// }
+  locationInput.addEventListener("change", function (e) {
+    console.log(e.target.value);
+    if (e.target.value === "台北市") {
+      for (let i = 0; i < 5; i++) {
+        if (i == 0 || i == 1) {
+          storeCards[i].style.display = "block";
+        } else {
+          storeCards[i].style.display = "none";
+        }
+      }
+    } else if (e.target.value === "台中市") {
+      for (let i = 0; i < 5; i++) {
+        if (i == 2) {
+          storeCards[i].style.display = "block";
+        } else {
+          storeCards[i].style.display = "none";
+        }
+      }
+    } else if (e.target.value === "高雄市") {
+      for (let i = 0; i < 5; i++) {
+        if (i == 3 || i == 4) {
+          storeCards[i].style.display = "block";
+        } else {
+          storeCards[i].style.display = "none";
+        }
+      }
+    } else {
+      for (let i = 0; i < 5; i++) {
+        storeCards[i].style.display = "block";
+      }
+    }
+
+    locationInput.addEventListener("click", function (e) {
+      if (e.target) {
+        e.target.value = "";
+      }
+    });
+  });
+}
